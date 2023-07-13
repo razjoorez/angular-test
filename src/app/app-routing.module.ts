e29f5537dd5e69cc3a0975e97149564a7b258484
reza razjoo <reza.razjoo@yahoo.com>
2023-07-13 18:59:13 +0100

    created regions component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'regions', loadChildren: () => import('./regions/regions.module').then(m => m.RegionsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
