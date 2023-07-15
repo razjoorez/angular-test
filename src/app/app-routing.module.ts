ec151e3f68fc30be16fd6e1ad67e2811689a8738
reza razjoo <reza.razjoo@yahoo.com>
2023-07-15 22:07:07 +0100

    fixed routing so initial page is regions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'regions', loadChildren: () => import('./regions/regions.module').then(m => m.RegionsModule) }, { path: 'country-details', loadChildren: () => import('./country-details/country-details.module').then(m => m.CountryDetailsModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./regions/regions.module').then(m => m.RegionsModule) }, { path: 'country-details', loadChildren: () => import('./country-details/country-details.module').then(m => m.CountryDetailsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
